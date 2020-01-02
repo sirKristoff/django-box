85e2a4b9c5ad2cd4b6dd87c00ec8a513fa60632b
Krzysztof Lasota <krzysztof.lasota@husqvarnagroup.com>
2020-01-02 20:28:58 +0100

    Extend admin view for Item

--- src/item/admin.py
+++ src/item/admin.py
@@ -5,6 +5,8 @@ from .models import Item
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
+    # list_editable = ('name',)  # which displayed fields are editable in list view
+    # list_display_links = ('id', 'name')  # which fields link to detail view
     list_filter = ('name',)
     search_fields = ('name', 'description')
     ordering = ('id',)
